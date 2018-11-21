49ead94614ccec9b1f363bdf8cf63ea149126127
ernesto gonzalez arellano <ergonzalezar@ittepic.edu.mx>
2018-11-21 02:53:35 -0700

    FEAT: Configuracion inicial del proyecto

---
+++ GestionPlaneacionDidacticaAPI/Data/DBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using GestionPlaneacionDidacticaAPI.Models;
+
+
+namespace GestionPlaneacionDidacticaAPI.Data
+{
+    public class DBContext : DbContext
+    {
+
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+
+    }
+}

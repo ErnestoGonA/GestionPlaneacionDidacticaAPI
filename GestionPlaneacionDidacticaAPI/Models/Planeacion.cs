ccc54ca7699db773eaaabe1d3553123b57b55b42
Bryan Gutiérrez Rojas <brgutierrezro@ittepic.edu.mx>
2018-12-16 03:45:30 -0700

    Fuentes

--- GestionPlaneacionDidacticaAPI/Models/Planeacion.cs
+++ GestionPlaneacionDidacticaAPI/Models/Planeacion.cs
@@ -59,10 +59,12 @@ namespace GestionPlaneacionDidacticaAPI.Models
     }
     public class eva_planeacion_fuentes
     {
-        public Int16 IdAsignatura { get; set; }
-        public int IdPlaneacion { get; set; }
+        [Key]
         public Int16 IdFuente { get; set; }
+        public int IdPlaneacion { get; set; }
+        public Int16 IdAsignatura { get; set; }
         public Int16 Prioridad { get; set; }
+        public string Observaciones { get; set; }
         public string UsuarioReg { get; set; }
         public DateTime FechaReg { get; set; }
         public DateTime FechaUltMod { get; set; }
@@ -167,7 +169,7 @@ namespace GestionPlaneacionDidacticaAPI.Models
         [Key]
         [Required]
         public Int16 IdFuente { get; set; }
-        public string DesFuenteCompetencia { get; set; }
+        public string DesFuenteCompleta { get; set; }
         public string Activo { get; set; }
         public string NombreFuente { get; set; }
         public string Autor { get; set; }

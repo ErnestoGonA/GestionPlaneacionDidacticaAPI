b8237c409ffe9e420b417e80e45f65c47e69565a
pablo <jopavalenzuelami@ittepic.edu.mx>
2018-12-17 09:10:41 -0700

    feat: Cambio en startup para manejar 2 bdcontext y que no se desmadre el proyecto

--- GestionPlaneacionDidacticaAPI/Startup.cs
+++ GestionPlaneacionDidacticaAPI/Startup.cs
@@ -32,6 +32,8 @@ namespace GestionPlaneacionDidacticaAPI
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<DBContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DBContext")));
+            services.AddDbContext<AlterDBContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DBContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
